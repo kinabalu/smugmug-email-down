fa4365c5cd0b554c0ee2d5f99453ef5da2790192
Andrew Lombardi <andrew@mysticcoders.com>
2013-06-12 12:20:21 -0700

    further refinements, done for now

--- gallerygrab.js
+++ gallerygrab.js
@@ -27,15 +27,43 @@ function openInbox(cb) {
     });
 }
 
-function processInbox(password) {
-    imap = new Imap({
-        user: args.imap.user,
-        password: password,
-        host: args.imap.host,
-        port: args.imap.port,
-        secure: args.imap.secure
-    });
+var download_assets = function(download_list) {
+    var shell = require('shelljs');
 
+    if(!shell.test('-e', 'smugmug')) {
+        shell.mkdir('smugmug');
+    }
+    shell.cd('smugmug');
+
+    console.log("Prepping download of up to %d galleries", download_list.length);
+    _.each(download_list, function(item, index, list) {
+        if(!item.skip && !item.done) {
+            var filename = item.url.substring(item.url.lastIndexOf('/') + 1);
+
+            console.log('Download gallery (%d) [%s] (%s) from %s', index + 1, item.gallery.trim(), item.size, item.url);
+
+            var normalized_gallery = item.gallery.replace(/[^a-zA-Zí\d\s:]/g, '');
+            normalized_gallery = normalized_gallery.trim();
+
+            if(!shell.test('-e', normalized_gallery)) {
+                console.log("%s doesn't exist, creating", normalized_gallery);
+                shell.mkdir(normalized_gallery);
+            }
+
+            shell.cd(normalized_gallery);
+
+            if(!shell.test('-e', util.format("%s.zip", filename))) {
+                var download = shell.exec(util.format('curl -L %s -o %s.zip', item.url, filename));
+            } else {
+                console.log("Appears that (%s) already exists so we will skip", filename);
+            }
+            shell.cd('..');
+            console.log();
+        }
+    });
+};
+
+var download_messages = function(callback) {
     download_list = [];
 
     openInbox(function(err, mailbox) {
@@ -64,7 +92,7 @@ function processInbox(password) {
                                     if(result) {
                                         download_list.push(
                                         {
-                                            gallery: result[1],
+                                            gallery: result[1].trim(),
                                             url: result[2],
                                             size: result[3]
                                         });
@@ -77,27 +105,56 @@ function processInbox(password) {
                     }, function(err) {
                         if(err) throw err;
                         imap.logout();
-                        var shell = require('shelljs');
 
-                        shell.mkdir('smugmug');
-                        shell.cd('smugmug');
-
-                        _.each(download_list, function(item) {
-
-                            var filename = item.url.substring(item.url.lastIndexOf('/') + 1);
-
-                            console.log('Download gallery [%s] (%s) from %s', item.gallery, item.size, item.url);
-
-                            shell.mkdir(item.gallery);
-                            shell.cd(item.gallery);
-                            var download = shell.exec(util.format('curl -L %s -o %s.zip', item.url, filename));
-                            shell.cd('..');
-                        });
+                        callback(download_list);
                     }
                 );
             });
         });
-    });    
+    });
+};
+
+function processInbox(password, save) {
+    imap = new Imap({
+        user: args.imap.user,
+        password: password,
+        host: args.imap.host,
+        port: args.imap.port,
+        secure: args.imap.secure
+    });
+
+    download_messages(function(download_list) {
+        if(save) {
+            writeDownloadListToFile(download_list);
+        }
+        download_assets(download_list);
+    });
+
+}
+
+function writeDownloadListToFile(download_list) {
+    var jsonText = JSON.stringify({list: download_list}, null, 4);
+
+    console.log(jsonText);
+
+    fs.writeFile("download.json", jsonText, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+}
+
+function processFile(file) {
+    var fileContents = fs.readFileSync(file,'utf8');
+    var schema = JSON.parse(fileContents);
+
+    if(schema && schema.list) {
+        download_assets(schema.list);
+    } else {
+        console.log("schema not properly formed.");
+    }
 }
 
 
@@ -105,11 +162,29 @@ var main = function() {
     program
         .version('0.0.1');
 
-    program.password(util.format('Enter IMAP password for [%s]:', args.imap.user), function(password) {
-        processInbox(password);
-    });
+    program
+        .command('email')
+        .option('-s --save', 'Save a json file for resuming')
+        .description('Read message from SmugMug in the INBOX before downloading')
+        .action(function(env) {
+            program.password(util.format('Enter IMAP password for [%s]:', args.imap.user), function(password) {
+                processInbox(password, env.save);
+            });
+        });
 
+    program
+        .command('resume')
+        .option('-f --file <jsonfile>', 'Resume from a saved JSON file from mail output')
+        .description('Resume from a previously saved JSON')
+        .action(function(env) {
+            if(env.file) {
+                processFile(env.file);
+            } else {
+                die('Specify a file with path');
+            }
+        });
 
+    program.parse(process.argv);
 };
 
 
